7d22e930a9e34452f778e60bcc3a04e79d0e0f87
Wiqiro <william.imbert@utbm.fr>
2022-05-10 18:08:18 +0200

    début du main (pas encore de gameplay)

--- main.c
+++ main.c
@@ -8,14 +8,46 @@
 
 
 int main(int argc, char* argv[]) {
-    int size = 8;
+
+    //on va implémenter des fonctions qui dépendent du système d'exploitation plus tard
     srand(time(NULL));
 
-    square** board = CreateBoard(size);
-    InitializeBoardClassic(board);
+
+    char gamemode;
+    printf("Bienvenue dans notre jeu d'échec !");
+    do {
+        printf("\nQuel mode de jeu choisissez-vous ? (B: Really bad chest, C: classic)\n");
+        scanf("%c",&gamemode);
+        gamemode = toupper(gamemode);
+    } while (gamemode != 'B' && gamemode != 'C');
+
+
+    square** board;
+    int size=0;
+    if (gamemode == 'C') {
+        size = 8;
+        board = CreateBoard(size);
+        InitializeBoardClassic(board);
+
+    } else {
+        char input[100];
+        do {
+            printf("\nChoisissez à présent la taille de l'échiquer (de 6x6 à 12x12)\n");
+            fflush(stdin);
+            scanf("%s",input);
+            size = strtol(input, NULL, 10);
+            
+        } while (size > 12 || size < 6);
+        board = CreateBoard(size);
+        InitializeBoardRandom(board, size);
+    }
+
+
+    
+    
+    
     SimplePrint(board, size);
 
-    //printf("\n%d",RookMoveTest(board,size,3,3,3,5));
 
 
 
